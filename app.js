ab0cfab20480038a51669252169c673ac6973219
Matheus Ribeiro :D <github.matheusribeiro@gmail.com>
2022-04-18 20:56:21 -0300

    declarando variaveis inicais

---
+++ app.js
@@ -0,0 +1,8 @@
+const form = document.querySelector('#form')
+const searchInput = document.querySelector('#search')
+const songsContainer = document.querySelector('#songs-container')
+const prevAndNextContainer = document.querySelector('#prev-and-next-container')
+
+const API_URL = `https://api.lyrics.ovh`
+
+console.log({ form,searchInput,songsContainer,prevAndNextContainer })
