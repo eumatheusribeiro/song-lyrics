70c26a1b021bfa779a1ca9381025c4b8f77a1d67
Matheus Ribeiro :D <github.matheusribeiro@gmail.com>
2022-04-18 21:07:55 -0300

    feat:tratando entrada com valor vazio

--- app.js
+++ app.js
@@ -5,4 +5,14 @@ const prevAndNextContainer = document.querySelector('#prev-and-next-container')
 
 const API_URL = `https://api.lyrics.ovh`
 
-console.log({ form,searchInput,songsContainer,prevAndNextContainer })
+form.addEventListener('submit', event => {
+    event.preventDefault()
+
+    const searchTerm = searchInput.value.trim()
+
+    if(!searchTerm) {
+    songsContainer.innerHTML = `<li class="warning-message">Por favor, digite um termo valido</li>`
+    }
+    
+    console.log(searchTerm)
+})
