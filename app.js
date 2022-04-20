5ecf7f4aa128d30d42bbe774cd4ca3d0c45da9bf
Gabriel Rodrigues <gabrirodri123@gmail.com>
2022-04-20 14:23:27 -0300

    retorno das musicas

--- app.js
+++ app.js
@@ -4,16 +4,22 @@ const songsContainer = document.querySelector('#songs-container')
 const prevAndNextContainer = document.querySelector('#prev-and-next-container')
 
 const API_URL = `https://api.lyrics.ovh`
+const insertSongsIntoPage = songsInfo => {
+    songsContainer.innerHTML = songsInfo.data.map(song =>
+        `<li class="song">
+        <span class="song-artist"><strong>${song.artist.name}</strong> - ${song.title}</span>
+        <button class="btn" data-artist="${song.artist.name}" data-song-title="${song.title}">Ver letra</button>
+        </li>
+        `).join('')
+}
 const fetchSongs = async term => {
     const response = await fetch(`${API_URL}/suggest/${term}`)
     const data = await response.json()
-    console.log(data)
+    insertSongsIntoPage(data)
 }
 form.addEventListener('submit', event => {
     event.preventDefault()
-
     const searchTerm = searchInput.value.trim()
-
     if (!searchTerm) {
         songsContainer.innerHTML = `<li class="warning-message">Por favor, digite um termo valido</li>`
         return
