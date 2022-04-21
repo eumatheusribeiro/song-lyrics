07d12713c9de9bfd43f8500e937e69218d9a29d8
Matheus Ribeiro :D <github.matheusribeiro@gmail.com>
2022-04-21 14:48:20 -0300

    feat:implementação botões de paginação

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const songsContainer = document.querySelector('#songs-container')
 const prevAndNextContainer = document.querySelector('#prev-and-next-container')
 
 const API_URL = `https://api.lyrics.ovh`
+
+const getMoreSongs = async url => {
+    const ab = url
+    const response = await fetch(`https://cors-anywhere.herokuapp.com/${ab}`)
+    const data = await response.json()
+    insertSongsIntoPage(data)
+}
+
 const insertSongsIntoPage = songsInfo => {
     songsContainer.innerHTML = songsInfo.data.map(song =>
         `<li class="song">
@@ -11,6 +19,16 @@ const insertSongsIntoPage = songsInfo => {
         <button class="btn" data-artist="${song.artist.name}" data-song-title="${song.title}">Ver letra</button>
         </li>
         `).join('')
+
+        if(songsInfo.prev || songsInfo.next) {
+            prevAndNextContainer.innerHTML = `
+                ${songsInfo.prev? `<button class="btn" onClick="getMoreSongs('${songsInfo.prev}')">Anteriores</button>`: ''}
+                ${songsInfo.next? `<button class="btn" onClick="getMoreSongs('${songsInfo.next}')">Próximas</button>`: ''}
+            `
+            return
+        }
+
+        prevAndNextContainer.innerHTML = ''
 }
 const fetchSongs = async term => {
     const response = await fetch(`${API_URL}/suggest/${term}`)
