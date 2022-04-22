88689c70c78e5cbfbab40556ef5342def94b3719
Gabriel Rodrigues <gabrirodri123@gmail.com>
2022-04-22 00:27:35 -0300

    refatorando o codigo - separando responsabilidades de cada função

--- app.js
+++ app.js
@@ -14,63 +14,69 @@ const getMoreSongs = async url => {
     const data = await fetchData(`https://cors-anywhere.herokuapp.com/${url}`)
     insertSongsIntoPage(data)
 }
-
-const insertSongsIntoPage = songsInfo => {
+const insertNextAndPrevButtons = ({ prev, next }) => {
+    prevAndNextContainer.innerHTML = `
+                ${prev ? `<button class="btn" onClick="getMoreSongs('${prev}')">Anteriores</button>` : ''}
+                ${next ? `<button class="btn" onClick="getMoreSongs('${next}')">Próximas</button>` : ''}
+            `
+}
+const insertSongsIntoPage = ({ data, prev, next }) => {
     document.querySelector('#loading').classList.add("not-active")
-    songsContainer.innerHTML = songsInfo.data.map(song =>
+    songsContainer.innerHTML = data.map(({ artist: { name }, title }) =>
         `<li class="song">
-        <span class="song-artist"><strong>${song.artist.name}</strong> - ${song.title}</span>
-        <button class="btn" data-artist="${song.artist.name}" data-song-title="${song.title}">Ver letra</button>
+        <span class="song-artist"><strong>${name}</strong> - ${title}</span>
+        <button class="btn" data-artist="${name}" data-song-title="${title}">Ver letra</button>
         </li>
         `).join('')
-
-        if(songsInfo.prev || songsInfo.next) {
-            prevAndNextContainer.innerHTML = `
-                ${songsInfo.prev? `<button class="btn" onClick="getMoreSongs('${songsInfo.prev}')">Anteriores</button>`: ''}
-                ${songsInfo.next? `<button class="btn" onClick="getMoreSongs('${songsInfo.next}')">Próximas</button>`: ''}
-            `
-            return
-        }
-
-        prevAndNextContainer.innerHTML = ''
+    if (prev || next) {
+        insertNextAndPrevButtons({ prev, next })
+        return
+    }
+    prevAndNextContainer.innerHTML = ''
 }
 const fetchSongs = async term => {
     const data = await fetchData(`${API_URL}/suggest/${term}`)
     insertSongsIntoPage(data)
 }
-form.addEventListener('submit', event => {
+const handleFormSubmit = event => {
     event.preventDefault()
     const searchTerm = searchInput.value.trim()
+    searchInput.value = ''
+    searchInput.focus()
     if (!searchTerm) {
         songsContainer.innerHTML = `<li class="warning-message">Por favor, digite um termo valido</li>`
         return
     }
-
     fetchSongs(searchTerm)
-})
+}
+
+form.addEventListener('submit', handleFormSubmit)
+
+const insertLyricsIntoPage = ({ lyrics, artist, songTitle }) => {
+    songsContainer.innerHTML = `
+        <li class="lyrics-container">
+            <h2><strong>${songTitle}</strong> - ${artist}</h2>
+            <p class="lyrics">${lyrics}</p>
+        </li>
+        `
+}
 
 const fetchLyrics = async (artist, songTitle) => {
     const data = await fetchData(`${API_URL}/v1/${artist}/${songTitle}`)
     const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, `<br>`)
-    
-    songsContainer.innerHTML = `
-     <li class="lyrics-container">
-      <h2><strong>${songTitle}</strong> - ${artist}</h2>
-      <p class="lyrics">${lyrics}</p>
-     </li>
-    `
+    insertLyricsIntoPage({ lyrics, artist, songTitle })
 }
-
-songsContainer.addEventListener('click', event => {
+const handleSongsContainerClick = event => {
     const clickedElement = event.target
-    if(clickedElement.tagName === 'BUTTON') {
+    if (clickedElement.tagName === 'BUTTON') {
         const artist = clickedElement.getAttribute('data-artist')
         const songTitle = clickedElement.getAttribute('data-song-title')
-        
+
         prevAndNextContainer.innerHTML = ''
         fetchLyrics(artist, songTitle)
     }
-})
+}
+songsContainer.addEventListener('click', handleSongsContainerClick)
 
 function colorTheme() {
     document.querySelector('body').classList.toggle("dark-theme")
